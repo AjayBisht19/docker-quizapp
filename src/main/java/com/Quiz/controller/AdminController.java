54a2ad12a545e27e20d7397c92f7bf21f5ea3280
AjayBisht19 <ajayb381@gmail.com>
2021-06-09 23:08:22 +0530

    cors origin to all

--- src/main/java/com/Quiz/controller/AdminController.java
+++ src/main/java/com/Quiz/controller/AdminController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
